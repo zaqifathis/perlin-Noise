1a30b64c08aabadee5840c47c6244f82c3e7bdc2
zqfths <zaqifathis@gmail.com>
2022-08-23 17:03:30 +0800

    update

--- sketch.js
+++ sketch.js
@@ -1,17 +1,14 @@
 let cols, rows;
 let zoff = 0;
 let colZoff = 0;
-
 const inc = 0.15;
 const scl = 20;
-const numParticles = 100;
-const points = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   cols = floor(width / scl);
   rows = floor(height / scl);
-  // noLoop();
+  noLoop();
 }
 
 function draw() {
@@ -20,7 +17,6 @@ function draw() {
   for (let y = 0; y < rows; y++) {
     let xoff = 0;
     for (let x = 0; x < cols; x++) {
-      let index = (x + y * width) * 4;
       let angle = noise(xoff, yoff, zoff) * TWO_PI;
       let v = p5.Vector.fromAngle(angle);
       xoff += inc;
@@ -40,12 +36,12 @@ function draw() {
   colZoff += 0.02;
 }
 
-// function keyTyped() {
-//   if (key === "a") {
-//     noLoop();
-//   }
-// }
+function keyTyped() {
+  if (key === "a") {
+    noLoop();
+  }
+}
 
-// function keyPressed() {
-//   loop();
-// }
+function keyPressed() {
+  loop();
+}
